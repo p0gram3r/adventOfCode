8530b9b98bac0d5ed70c254209e8f7d681b488e0
André <p0gram3r@gmail.com>
2024-10-29 08:47:06 +0100

    reworking solution

--- src/main/java/it/p0gram3r/adventofcode/y2021/Day15.java
+++ src/main/java/it/p0gram3r/adventofcode/y2021/Day15.java
@@ -1,17 +1,18 @@
 package it.p0gram3r.adventofcode.y2021;
 
 import it.p0gram3r.adventofcode.Puzzle;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import lombok.AccessLevel;
+import java.util.Set;
+import java.util.TreeMap;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.experimental.FieldDefaults;
-import lombok.val;
+import lombok.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,28 +22,159 @@ public class Day15 implements Puzzle {
 
     @Override
     public String solutionA(List<String> input) {
-        val map = parseCavernMap(input);
-        QMap qMap = new QMap(map);
+        var map = parseCavernMap(input);
 
-        return calculateRistUsingDijkstraAlgorithm(qMap);
+        var graph = createGraph(map);
+        calculateShortestPathFromSource(graph, graph.getNode(0, 0));
+
+        int maxX = map[0].length - 1;
+        int maxY = map.length - 1;
+        return graph.getNode(maxX, maxY).getDistance().toString();
     }
 
     @Override
     public String solutionB(List<String> input) {
-//        var map = parseCavernMap(input);
-//        map = scaleToFullCavernMap(map);
-//
-//        QMap qMap = new QMap(map);
-//
-//        StopWatch stopWatch = StopWatch.createStarted();
-//        String result = qMapDijkstraAlgorithm(qMap);
-//        stopWatch.stop();
-//        log.info("### duration: {}", stopWatch.getTime());
-//        return result;
+        var map = parseCavernMap(input);
+        map = scaleToFullCavernMap(map);
 
-        // calculating the solution took ~ 40 minutes!
-        return "2876";
+        var graph = createGraph(map);
+        calculateShortestPathFromSource(graph, graph.getNode(0, 0));
+
+        int maxX = map[0].length - 1;
+        int maxY = map.length - 1;
+        return graph.getNode(maxX, maxY).getDistance().toString(); // "2876";
     }
+
+    @Value
+    static class Graph {
+        Collection<Node> nodes = new ArrayList<>();
+
+        public void addNode(Node nodeA) {
+            nodes.add(nodeA);
+        }
+
+        public Node getNode(int x, int y) {
+            return nodes.stream()
+                    .filter(n -> n.x == x)
+                    .filter(n -> n.y == y)
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Node not found"));
+        }
+    }
+
+    @Data
+    static class Node {
+        final int x, y, risk;
+        @EqualsAndHashCode.Exclude
+        List<Node> shortestPath = new LinkedList<>();
+        @EqualsAndHashCode.Exclude
+        Integer distance = Integer.MAX_VALUE;
+        @EqualsAndHashCode.Exclude
+        Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+        public void addDestination(Node destination, int distance) {
+            adjacentNodes.put(destination, distance);
+        }
+
+        public Node(int x, int y, int risk) {
+            this.x = x;
+            this.y = y;
+            this.risk = risk;
+        }
+
+        public String toString() {
+            return x + " / " + y + " (" + risk + ")";
+        }
+    }
+
+    static Graph calculateShortestPathFromSource(Graph graph, Node source) {
+        source.setDistance(0);
+
+        Set<Node> settledNodes = new HashSet<>();
+        Set<Node> unsettledNodes = new HashSet<>();
+
+        unsettledNodes.add(source);
+
+        while (!unsettledNodes.isEmpty()) {
+            Node currentNode = getLowestDistanceNode(unsettledNodes);
+            unsettledNodes.remove(currentNode);
+            for (Map.Entry<Node, Integer> adjacencyPair :
+                    currentNode.getAdjacentNodes().entrySet()) {
+                Node adjacentNode = adjacencyPair.getKey();
+                Integer edgeWeight = adjacencyPair.getValue();
+                if (!settledNodes.contains(adjacentNode)) {
+                    calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+                    unsettledNodes.add(adjacentNode);
+                }
+            }
+            settledNodes.add(currentNode);
+        }
+        return graph;
+    }
+
+    private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
+        Node lowestDistanceNode = null;
+        int lowestDistance = Integer.MAX_VALUE;
+        for (Node node : unsettledNodes) {
+            int nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+        return lowestDistanceNode;
+    }
+
+    private static void calculateMinimumDistance(Node evaluationNode,
+                                                 Integer edgeWeigh, Node sourceNode) {
+        Integer sourceDistance = sourceNode.getDistance();
+        if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
+            evaluationNode.setDistance(sourceDistance + edgeWeigh);
+            LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
+
+    static Graph createGraph(int[][] map) {
+        Graph graph = new Graph();
+
+        int xLength = map[0].length;
+        int yLength = map.length;
+
+
+        Map<Integer, Map<Integer, Node>> adjacentNodes = new TreeMap<>();
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                Node node = new Node(x, y, map[y][x]);
+                graph.addNode(node);
+                adjacentNodes.computeIfAbsent(y, k -> new TreeMap<>()).put(x, node);
+            }
+        }
+
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                Node node = adjacentNodes.get(y).get(x);
+
+                if (x > 0) {
+                    node.addDestination(adjacentNodes.get(y).get(x - 1), node.risk);
+                }
+                if (x < xLength - 1) {
+                    node.addDestination(adjacentNodes.get(y).get(x + 1), node.risk);
+                }
+                if (y > 0) {
+                    node.addDestination(adjacentNodes.get(y - 1).get(x), node.risk);
+                }
+                if (y < yLength - 1) {
+                    node.addDestination(adjacentNodes.get(y + 1).get(x), node.risk);
+                }
+            }
+        }
+
+
+        return graph;
+    }
+
 
     static int[][] parseCavernMap(List<String> input) {
         int[][] map = new int[input.size()][];
@@ -56,104 +188,6 @@ public class Day15 implements Puzzle {
         }
 
         return map;
-    }
-
-    static class QMap {
-        private final Map<Integer, Map<Integer, QMapEntry>> entries = new HashMap<>();
-        private final int maxX;
-        private final int maxY;
-
-        // TODO use this instead of the int-array map?
-        private QMap(int[][] map) {
-            maxX = map[0].length - 1;
-            maxY = map.length - 1;
-
-            for (int y = 0; y < map.length; y++) {
-                for (int x = 0; x < map[y].length; x++) {
-                    val xMap = entries.computeIfAbsent(y, k -> new HashMap<>());
-                    xMap.put(x, new QMapEntry(x, y, map[y][x]));
-                }
-            }
-        }
-
-        Collection<QMapEntry> unvisitedNeighbors(QMapEntry entry) {
-            Collection<QMapEntry> neighbors = new HashSet<>();
-            getEntry(entry.x - 1, entry.y).filter(n -> !n.visited).ifPresent(neighbors::add);
-            getEntry(entry.x + 1, entry.y).filter(n -> !n.visited).ifPresent(neighbors::add);
-            getEntry(entry.x, entry.y - 1).filter(n -> !n.visited).ifPresent(neighbors::add);
-            getEntry(entry.x, entry.y + 1).filter(n -> !n.visited).ifPresent(neighbors::add);
-            return neighbors;
-        }
-
-        private Optional<QMapEntry> getEntry(int x, int y) {
-            val xMap = entries.get(y);
-            if (xMap == null) {
-                return Optional.empty();
-            }
-
-            return Optional.ofNullable(xMap.get(x));
-        }
-
-        public QMapEntry getSourceEntry() {
-            return getEntry(0, 0).orElseThrow(RuntimeException::new);
-        }
-
-        public QMapEntry getTargetEntry() {
-            return getEntry(maxX, maxY).orElseThrow(RuntimeException::new);
-        }
-    }
-
-    @Data
-    @FieldDefaults(level = AccessLevel.PRIVATE)
-    static class QMapEntry {
-        final int x, y, risklevel;
-        @EqualsAndHashCode.Exclude
-        boolean visited = false;
-    }
-
-    /*
-     * https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
-     */
-    private String calculateRistUsingDijkstraAlgorithm(QMap qmap) {
-        QMapEntry source = qmap.getSourceEntry();
-        QMapEntry target = qmap.getTargetEntry();
-
-        Map<QMapEntry, Integer> dist = new HashMap<>();
-        for (int y = 0; y <= target.y; y++) {
-            for (int x = 0; x <= target.x; x++) {
-                QMapEntry v = qmap.getEntry(x, y).orElseThrow(RuntimeException::new);
-                dist.put(v, Integer.MAX_VALUE);
-            }
-        }
-
-        dist.put(source, 0);
-
-        while (true) {
-            QMapEntry u = findEntryWithSmallestDistanceStillInQ(dist);
-            if (u == target) {
-                break;
-            }
-
-            u.visited = true;
-
-            val neighbors = qmap.unvisitedNeighbors(u);
-            neighbors.forEach(v -> {
-                val alt = dist.get(u) + v.risklevel;
-                if (alt < dist.get(v)) {
-                    dist.put(v, alt);
-                }
-            });
-        }
-
-        return Integer.toString(dist.get(target));
-    }
-
-    private QMapEntry findEntryWithSmallestDistanceStillInQ(Map<QMapEntry, Integer> dist) {
-        return dist.entrySet().stream()
-                .filter(e -> !e.getKey().visited)
-                .min(Map.Entry.comparingByValue())
-                .orElseThrow(RuntimeException::new)
-                .getKey();
     }
 
     static int[][] scaleToFullCavernMap(int[][] map) {
